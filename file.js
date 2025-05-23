c21156e71984daa5699e81057420afb074cfd1d7
muntahaasif <muntahaasif82@gmail.com>
2025-05-23 10:20:14 -0700

    Add files via upload

---
+++ file.js
@@ -0,0 +1,45 @@
+
+// FIRST EXERCISE QUIZ
+// ques:1
+var  popup = "alert";
+alert("correct!");
+
+// ques:2
+ var display = "semicolon";
+ alert(";");
+
+//  ques:3
+var check = "parentheses";
+alert("()");
+
+// ques:4
+var correct = "statement";
+alert("alert!");
+
+// ques:5
+var done = "alert"
+alert("Thanks for everything!");
+
+// ques:6
+var alert = "boo";
+alert("Boo!");
+
+// ques:7
+var input = "alert";
+alert("alert");
+
+// ques:8
+var output = "won"
+alert();
+
+// ques:9
+var last = "order"
+alert("");
+
+// ques:10
+var last = "last"
+alert(".");
+
+
+
+
